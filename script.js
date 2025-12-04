7df61399894341ee84b31c2a7360b931be40c825
Jonas Gonzales <jgonz466@asu.edu>
2025-12-04 14:00:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 const movies = [
     {
         name: "The Last Sunset",
-        image: "images/Sunset.jpg",
+        image: "Sunset.jpg",
         desc: "A heartwarming drama about family and redemption. Follow the journey of a retired fisherman rediscovering life's meaning. Rated PG-13. Showtimes: 2:00 PM, 5:00 PM, 8:00 PM"
     },
