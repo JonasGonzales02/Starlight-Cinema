8d36898bdf7dd50844e4f8ef2e779d1adef929a7
Jonas Gonzales <jgonz466@asu.edu>
2025-12-04 14:02:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ const movies = [
         image: "Sunset.jpg",
         desc: "A heartwarming drama about family and redemption. Follow the journey of a retired fisherman rediscovering life's meaning. Rated PG-13. Showtimes: 2:00 PM, 5:00 PM, 8:00 PM"
     },
-]
+];
