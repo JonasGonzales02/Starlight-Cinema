a951eb42618e984dcdac2c222d754dc40ba22599
Jonas Gonzales <jgonz466@asu.edu>
2025-12-04 14:18:51 -0700

    images

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const movies = [
         image: "images/Sunset.jpg",
         desc: "A heartwarming drama about family and redemption. Follow the journey of a retired fisherman rediscovering life's meaning. Rated PG-13. Showtimes: 2:00 PM, 5:00 PM, 8:00 PM"
     },
+    {
+        name: "Galaxy Quest",
+        image: "images/Galaxy.jpg",
+        desc: "Epic space adventure that will take you to new worlds. Join the crew of the Starship Endeavor as they battle alien forces to save humanity. Rated PG. Showtimes: 3:00 PM, 6:00 PM, 9:00 PM"
+    },
+    {
+        name: "Midnight in Paris",
+        image: "images/Paris.jpg",
+        desc: "Romantic comedy that will make you believe in love again. Two strangers meet by chance and embark on a night through the city of lights. Rated PG-13. Showtimes: 4:00 PM, 7:00 PM"
+    }
 ];
